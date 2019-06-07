af9b3b86896ec91f5e2a02ba4eb124e4a45966df
RED LOCAL KIMERA <51243585+RED-LOCAL-KIMERA@users.noreply.github.com>
2019-06-07 14:40:47 -0500

    VERSION_1.0

---
+++ VERSION_BETA/KIMERA_RL/RED_LOCAL/xampp/htdocs/moodle/config.php
@@ -0,0 +1,23 @@
+<?php  // Moodle configuration file 
+unset($CFG); 
+global $CFG; 
+$CFG = new stdClass(); 
+$CFG->dbtype    = 'mariadb'; 
+$CFG->dblibrary = 'native'; 
+$CFG->dbhost    = 'localhost'; 
+$CFG->dbname    = 'moodle'; 
+$CFG->dbuser    = 'root'; 
+$CFG->dbpass    = ''; 
+$CFG->prefix    = 'mdl_'; 
+$CFG->dboptions = array ( 
+  'dbpersist' => 0, 
+  'dbport' => '', 
+  'dbsocket' => '', 
+); 
+$CFG->wwwroot   = 'http://192.168.20.3/RED_LOCAL/xampp/htdocs/moodle'; 
+$CFG->dataroot  = 'C:\\KIMERA_RL\\RED_LOCAL\\xampp\\moodledata'; 
+$CFG->admin     = 'admin'; 
+$CFG->directorypermissions = 0777; 
+require_once(dirname(__FILE__) . '/lib/setup.php'); 
+// There is no php closing tag in this file, 
+// it is intentional because it prevents trailing whitespace problems 
